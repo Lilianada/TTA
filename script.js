1f0b3a580b56c211cfea729dec0e4aedd7eb500f
Lilianada <lilibest30.lb@gmail.com>
2021-05-29 02:16:30 +0100

    enter

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+
+const menu= document.querySelector (".hamburger");
+const nav= document.querySelector (".navlinks");
+
+menu.addEventListener("click", () => {   //this makes the hamburger change from the three lines to the times button,
+ nav.classList.toggle("ham")             // when the selector ham is added in front of the lines.
+})                                       //toggle means switch from one thing to another
+   
